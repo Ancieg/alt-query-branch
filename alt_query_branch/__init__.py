8c34e5dd96892bbf92402dff8fb7c79b767f2714
Anton Zhukharev <ancieg@altlinux.org>
2022-12-26 00:04:42 +0300

    add FoundPackage datastructure

---
+++ alt_query_branch/__init__.py
@@ -0,0 +1,8 @@
+ordered_arches = [
+    'noarch',
+    'x86_64',
+    'i586',
+    'armh',
+    'aarch64',
+    'ppc64le'
+]
---
+++ alt_query_branch/datastructures.py
@@ -0,0 +1,36 @@
+from . import ordered_arches
+
+class FoundPackage:
+    def __init__(self, source):
+        self._source = source
+        self._binaries = []
+
+    def add_bin(self, arch, bin):
+        if {'arch': arch, 'name': bin} not in self._binaries:
+            self._binaries.append({'arch': arch, 'name': bin})
+
+    def _sorted_arches(self):
+        arches = set()
+        for bin in self._binaries:
+            arches.add(bin['arch'])
+        arches = list(arches)
+
+        ordered_arches_sub = []
+        for arch in ordered_arches:
+            if arch in arches:
+                ordered_arches_sub.append(arch)
+        return ordered_arches_sub
+
+    def _sort_binaries(self):
+        sorted_bin_packages = []
+        for arch in self._sorted_arches():
+            for bin in sorted(filter(lambda x: x['arch'] == arch, self._binaries), key=lambda bin: bin['name']):
+                sorted_bin_packages.append({'arch': arch, 'name': bin['name']})
+        self._binaries = sorted_bin_packages
+
+    def to_dict(self):
+        self._sort_binaries()
+        return {
+            "source": self._source,
+            "binaries": self._binaries
+        }
