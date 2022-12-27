896fffaf1f20a56925ac8dc9937833954993aadb
Anton Zhukharev <ancieg@altlinux.org>
2022-12-28 02:00:19 +0300

    get rid of cli.py

--- alt_query_branch/entrypoint.py
+++ alt_query_branch/entrypoint.py
@@ -1,15 +1,26 @@
 import json
 import sys
+import argparse
 
-from .cli import argparser, args
 from .core_algorithms import search_binary_packages, sort_with_order
 from .datastructures import Result
+from .constants import ORDERED_ARCHES
 
 def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-b', '--branch', type=str, default='sisyphus')
+    argparser.add_argument('-a', '--arches', type=str, default=','.join(ORDERED_ARCHES))
+    argparser.add_argument('-e', '--exact', action='store_true')
+    argparser.add_argument('-s', '--stdout', action='store_true')
+    argparser.add_argument('-o', '--file', type=str)
+    argparser.add_argument('expression')
+
+    args = vars(argparser.parse_args())
+
     match  = args['expression']
     exact  = args['exact']
     branch = args['branch']
-    arches = args['arches']
+    arches = args['arches'].split(',')
     file   = args['file']
     stdout = args['stdout']
 
