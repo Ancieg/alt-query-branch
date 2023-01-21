31c89d8dd78e5b7239276b3c29d60bc39fd89cd4
Anton Zhukharev <ancieg@altlinux.org>
2023-01-22 01:48:23 +0300

    simplify BinaryPackage class

--- alt_query_branch/datastructures.py
+++ alt_query_branch/datastructures.py
@@ -15,29 +15,26 @@ class BinaryPackage:
 
 class SourcePackage:
     def __init__(self, source: str):
-        self._source: str = source
-        self._binaries: list[BinaryPackage] = []
+        self._source = source
+        self._binaries = set()
 
     def add_bin(self, package: BinaryPackage):
-        if package not in self._binaries:
-            self._binaries.append(package)
+        self._binaries.add(package)
 
     def _ordered_arches(self):
         arches = list({bin.arch for bin in self._binaries})
         return [arch for arch in ORDERED_ARCHES if arch in arches]
 
-    def _order_binaries(self):
+    def _ordered_binaries(self):
         ordered_binaries = []
         for arch in self._ordered_arches():
             one_arch_binaries = filter(lambda x: x.arch == arch, self._binaries)
             ordered_one_arch_binaries = sorted(one_arch_binaries, key=lambda bin: bin.name)
             ordered_binaries.extend(ordered_one_arch_binaries)
-        self._binaries = ordered_binaries
+        return ordered_binaries
 
     def to_dict(self):
-        self._order_binaries()
         return {
             "source": self._source,
-            "binaries": [asdict(b) for b in self._binaries]
+            "binaries": [asdict(b) for b in self._ordered_binaries()]
         }
-
