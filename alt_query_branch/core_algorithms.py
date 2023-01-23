ef377dd2ff757208602c15371906983f1d2aabd3
Anton Zhukharev <ancieg@altlinux.org>
2023-01-23 04:44:17 +0300

    simplify order_packages() function

--- alt_query_branch/core_algorithms.py
+++ alt_query_branch/core_algorithms.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import jq
 
 import alt_query_branch.rdb as rdb
@@ -6,23 +8,20 @@ from .constants import ORDERED_ARCHES
 from .datastructures import BinaryPackage, SourcePackage
 
 
-def _order_packages(packages: dict):
+def order_packages(packages: list[dict[str, Any]]) -> list[dict[str, Any]]:
     def without_keys(d, keys):
         return {x: d[x] for x in d if x not in keys}
 
-    sorted_packages = sorted(packages, key=lambda d: d['source'])
-    source_packages_names = list(set(s['source'] for s in sorted_packages))
+    sources_names = list({s['source'] for s in packages})
+    sources = {sn: SourcePackage(sn) for sn in sources_names}
 
-    source_packages: list[SourcePackage] = []
+    for package in packages:
+        sources[package['source']].add_bin(
+            BinaryPackage(**without_keys(package, 'source'))
+        )
 
-    for source_package_name in sorted(source_packages_names):
-        binaries = filter(lambda s: s['source'] == source_package_name, sorted_packages)
-        source_package = SourcePackage(source_package_name)
-        for binary in binaries:
-            source_package.add_bin(BinaryPackage(**without_keys(binary, 'source')))
-        source_packages.append(source_package)
+    return [v.to_dict() for k, v in sorted(sources.items())]
 
-    return source_packages
 
 def search_matching_packages(match, exact=False, branch='sisyphus', arches='all'):
     """
@@ -41,6 +40,5 @@ def search_matching_packages(match, exact=False, branch='sisyphus', arches='all'
         expr += ' | select(.source == "{}" or .name == "{}")'.format(match, match)
 
     plain_result = jq.jq(expr).transform(packages, multiple_output=True)
-    ordered_result = _order_packages(plain_result)
 
-    return [p.to_dict() for p in ordered_result]
+    return order_packages(plain_result)
